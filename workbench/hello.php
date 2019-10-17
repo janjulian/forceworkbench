63f4cc12956bcd4d1ec7eab8c7ef079d493bc4ad
Jan Julian <jan@graycloud.ca>
2019-10-17 13:26:58 -0700

    remove shared.php

--- workbench/hello.php
+++ workbench/hello.php
@@ -2,11 +2,11 @@
 require_once 'session.php';
 // require_once 'shared.php';
 // include_once 'header.php';
-require_once 'footer.php';
+// require_once 'footer.php';
 ?>
 
 <?php echo 'hello' ?>
 
 <?php
-include_once 'footer.php';
+// include_once 'footer.php';
 ?>
