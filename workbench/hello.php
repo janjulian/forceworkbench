933ea5d1438e9df853314a51577239bf0db596f5
Jan Julian <jan@graycloud.ca>
2019-11-01 14:54:36 -0700

    Adjust logs.

--- workbench/hello.php
+++ workbench/hello.php
@@ -6,7 +6,6 @@ include_once 'header.php';
 
 <textarea id="requestHeaders" name="requestHeaders" style="width: 100%; height: 500px; font-family: courier, monotype;">
 <?php 
-echo "Sfdx:\n";
 echo shell_exec('bash ./../sfdx_scripts/sfdx_run');
 ?>
 </textarea>
