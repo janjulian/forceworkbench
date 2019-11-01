d3ba6da4d258531bff27c4e33c604eecee3773d2
Jan Julian <jan@graycloud.ca>
2019-11-01 13:01:46 -0700

    Adjust height

--- workbench/hello.php
+++ workbench/hello.php
@@ -4,7 +4,7 @@ require_once 'shared.php';
 include_once 'header.php';
 ?>
 
-<textarea id="requestHeaders" name="requestHeaders" style="width: 100%; height: 600px; font-family: courier, monotype;">
+<textarea id="requestHeaders" name="requestHeaders" style="width: 100%; height: 500px; font-family: courier, monotype;">
 <?php 
 echo "Sfdx:\n";
 echo shell_exec('bash ./../sfdx_scripts/sfdx_org_list');
